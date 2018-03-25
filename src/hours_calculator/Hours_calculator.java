7c9f7b7a595aca5d11f2b2c4c5a6b985bcd2eee5
bsergiub <37773576+bsergiub@users.noreply.github.com>
2018-03-25 22:22:45 +0100

    refactored.

--- src/hours_calculator/Hours_calculator.java
+++ src/hours_calculator/Hours_calculator.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hours_calculator;
 
 /**
@@ -16,7 +12,6 @@ public class Hours_calculator {
     public static void main(String[] args) {
         HoursCalcGUI app = new HoursCalcGUI();
         app.setVisible(true);
-        CoreCalc calculator = new CoreCalc();
     }
   
 
